db9a8ca01a57dbec87f09567e2f304c424e51580
Patrice Blin <valar.morghulis.dohaeris@gmail.com>
2013-12-12 00:23:38 +0100

    more correct usb device pick up.

--- src/usb.c
+++ src/usb.c
@@ -4,39 +4,87 @@
 #define VENDOR_ID 0x2304 /* Pinnacle Systems, Inc. */
 #define PRODUCT_ID 0x0223 /* DazzleTv Sat DBA Device */
 
-int main()
+int check_vidpid(struct libusb_device *dev)
 {
 	int rc;
+	struct libusb_device_descriptor desc;
+	if ((rc = libusb_get_device_descriptor(dev, &desc)) < 0) {
+		printf("libusb_get_device_descriptor failed: %s\n", libusb_strerror(rc));
+		return -1;
+	}
+	if (desc.idVendor == VENDOR_ID && desc.idProduct == PRODUCT_ID)
+		return 0;
+	return -2;
+}
+
+int main()
+{
+	int i, rc, active = 0;
+	size_t size;
 	struct libusb_context *ctx;
+	struct libusb_device **list;
+	struct libusb_device *dev, *found;
 	static struct libusb_device_handle *h;
 
 	if ((rc = libusb_init(&ctx)) < 0) {
 		printf("libusb_init failed: %s\n", libusb_strerror(rc));
-		return rc;
+		goto exit;
 	}
 
 	libusb_set_debug(ctx, LIBUSB_LOG_LEVEL_DEBUG);
 
-	h = libusb_open_device_with_vid_pid(ctx, VENDOR_ID, PRODUCT_ID);
-	if (!h) {
-		printf("libusb_open_device(0x%04x, 0x%04x) failed\n", VENDOR_ID, PRODUCT_ID);
+	if ((size = libusb_get_device_list(ctx, &list)) < 0) {
+		printf("libusb_get_device_list failed: %s\n", libusb_strerror(size));
 		return -1;
+	}
+
+	found = NULL;
+	for (i = 0; i < size; i++) {
+		dev = list[i];
+		if (check_vidpid(dev) == 0) {
+			found = dev;
+			break;
+		}
+	}
+
+	if (!found) {
+		printf("device(vid=0x%04x, pid=0x04%x) not found", VENDOR_ID, PRODUCT_ID);
+		goto free;
+	}
+
+	if ((rc = libusb_open(found, &h)) < 0) {
+		printf("libusb_open failed: %s\n", libusb_strerror(rc));
+		goto free;
+	}
+
+	if (libusb_kernel_driver_active(h, 0)) {
+		printf("device busy\n");
+		libusb_detach_kernel_driver(h, 0);
+		active = 1;
 	}
 
 	if ((rc = libusb_claim_interface(h, 0)) < 0) {
 		printf("libusb_claim_interface failed: %s\n", libusb_strerror(rc));
-		return rc;
+		goto close;
 	}
 
 	sleep(10);
 
+	/* XXX: WTF? */
+	if (active)
+		libusb_attach_kernel_driver(h, 0);
+
+
 	if ((rc = libusb_release_interface(h, 0)) < 0) {
 		printf("libusb_release_interface failed: %s\n", libusb_strerror(rc));
-		return rc;
 	}
 
+close:
 	libusb_close(h);
+free:
+	libusb_free_device_list(list, 1);
+exit:
 	libusb_exit(ctx);
 
-	return 0;
+	return rc;
 }
